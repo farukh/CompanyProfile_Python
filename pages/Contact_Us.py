dee07217b08c022c289609f9c731c607e32ab9a5
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-13 12:33:47 +0500

    - Contact Us Form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,2 +1,27 @@
+import time
+import pandas
+
 import streamlit as st
-st.title('Contact Us')
+from send_email import send_email
+topics = pandas.read_csv('topics.csv')
+st.title("Contact Us")
+with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address:")
+    option = st.selectbox(
+        "What topic do you want to discuss?",
+        topics)
+    raw_message = st.text_area("Your Message: ")
+
+    message = f"""Subject: Company Profile App, {user_email}, {option}\n
+
+        From: {user_email}
+        Topic: {option}
+        Sent Following Message: 
+        {raw_message}
+            """
+    btn_submit = st.form_submit_button("Submit")
+    print(btn_submit)
+    if btn_submit:
+        msg = st.info("Please wait... \n Email being sent. ")
+        send_email(message)
+        msg.info("Email Sent Successfully. ")
---
+++ send_email.py
@@ -0,0 +1,28 @@
+import smtplib, ssl
+import os
+
+def send_email(message):
+
+    host = "smtp.gmail.com"
+    port = 465
+    username = "farukh.mushtaq@gmail.com"
+    #password = "gnqppvdkkavappzq"
+    password = os.getenv("PASSWORD")
+    context = ssl.create_default_context()
+    receiver = "farukh.mushtaq@gmail.com"
+    # with smtplib.SMTP_SSL(host, port, context=context) as server:
+    #     server.login(username,password)
+    #     server.sendmail(username, receiver, message)
+    print(f'Password from Env. {os.getenv("PASSWORD")}')
+
+if __name__ == '__main__':
+    raw_message = " i wnat to meet you"
+    receiver_email = "pakipcs@yahoo.com"
+    message = f"""Subject: Email Portfolio App\n
+    
+    Sent Following Message: {raw_message}
+    From: {receiver_email}
+        """
+    print("Sending Email...")
+    send_email(message)
+    print("Email Sent...")
